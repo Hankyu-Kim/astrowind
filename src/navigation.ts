fddbcdd63cd4e683594298cc6ded6a7c3e0d158f
Hankyu Kim <tim4688@naver.com>
2025-08-28 14:10:00 +0900

    dynamics.mdx added

--- src/navigation.ts
+++ src/navigation.ts
@@ -24,36 +24,40 @@ export const headerData = {
       ],
     },
     {
-      text: 'Dynamics',
+      text: 'System Modeling',
       links: [
+        // {
+        //   text: 'Features (Anchor Link)',
+        //   href: getPermalink('/#features'),
+        // },
+        // {
+        //   text: 'Services',
+        //   href: getPermalink('/services'),
+        // },
+        // {
+        //   text: 'Pricing',
+        //   href: getPermalink('/pricing'),
+        // },
+        // {
+        //   text: 'About us',
+        //   href: getPermalink('/about'),
+        // },
+        // {
+        //   text: 'Contact',
+        //   href: getPermalink('/contact'),
+        // },
+        // {
+        //   text: 'Terms',
+        //   href: getPermalink('/terms'),
+        // },
+        // {
+        //   text: 'Privacy policy',
+        //   href: getPermalink('/privacy'),
+        // },
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
+          text: 'Dynamics',
+          href: getPermalink('dynamics', 'post'),
+        }
       ],
     },
     {
