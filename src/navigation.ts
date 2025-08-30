801dfb0d97e8549ec7d3ffb7334641b8881843db
Hankyu Kim <tim4688@naver.com>
2025-08-31 00:03:06 +0900

    add socialLinks

--- src/navigation.ts
+++ src/navigation.ts
@@ -145,11 +145,11 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/hankyu_kim' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/hankyu.kim' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/profile.php?id=61553479105638' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/Hankyu-Kim' },
   ],
   footNote: `
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/Hankyu-Kim"> Hankyu Kim</a> · All rights reserved.
